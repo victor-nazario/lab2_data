50dffa9106abc194f82906e1f1f057ff20b3dfef
victor-nazario <33332105+victor-nazario@users.noreply.github.com>
2018-03-07 20:20:47 -0400

    Final commit, bug fixes and improvements to code.

--- src/arrayIndexList/ArrayIndexList.java
+++ src/arrayIndexList/ArrayIndexList.java
@@ -1,7 +1,6 @@
 package arrayIndexList;
 
 import java.lang.reflect.Array;
-import java.util.Arrays;
 
 import indexList.IndexList;
 
@@ -19,30 +18,31 @@ public class ArrayIndexList<E> implements IndexList<E> {
 	
 
 	public void add(int index, E e) throws IndexOutOfBoundsException {
-		if (index < 0 || element.length < index) { throw new IndexOutOfBoundsException("The index value is not valid");}
-		if (element.length - size > MAXEMPTYPOS) {
+		if(index < 0 || index > this.size) {
+			throw new IndexOutOfBoundsException("add: invalid index = " + index);
+		}
+		if(this.size == element.length) {
 			this.changeCapacity(CAPTOAR);
 		}
-		else {
-			size++;
-			this.moveDataOnePositionTR(index+1, size);
-			element[index] = (E)e; 
-		 
-		}
+		moveDataOnePositionTR(index, this.size-1);
+		element[index] = e;
+		this.size++;
 	}
 
 
 	public void add(E e) {
-		if (element.length - size > MAXEMPTYPOS) {
+		if(this.size == element.length) {
 			this.changeCapacity(CAPTOAR);
 		}
-		element[element.length] = (E)e; 
-		size++;
+		element[this.size] = e;
+		this.size++;
 	}
 
 
 	public E get(int index) throws IndexOutOfBoundsException {
-		if (index < 0 || element.length < index) { throw new IndexOutOfBoundsException("The index value is not valid");}
+		if(index < 0 || index > this.size) {
+			throw new IndexOutOfBoundsException("get: invalid index = " + index);
+		}	
 		return element[index]; 
 	}
 
@@ -53,29 +53,34 @@ public class ArrayIndexList<E> implements IndexList<E> {
 
 
 	public E remove(int index) throws IndexOutOfBoundsException {
-		if (index < 0 || element.length -1 < index) { throw new IndexOutOfBoundsException("The index value is not valid");}
-			E etr = element[index]; 
-			size--;
-			this.moveDataOnePositionTL(index, size);
-			
-		return etr;
+		if(index < 0 || index > this.size-1) {
+			throw new IndexOutOfBoundsException("remove: invalid index = " + index);
+		}
+		if(MAXEMPTYPOS <= (element.length - this.size)){
+			this.changeCapacity(-CAPTOAR);
+		}
+		E toBeReturned = element[index];
+		moveDataOnePositionTL(index+1, this.size-1);
+		element[size-1] = null;
+		this.size--;
+		return toBeReturned;
 	}
 
 
 	public E set(int index, E e) throws IndexOutOfBoundsException {
-		if (index < 0 || element.length < index) { throw new IndexOutOfBoundsException("The index value is not valid");}
-		element[index] = (E)e;
-		element[element.length+1] = element[index]; 
 		
-		return element[element.length+1];
-		
+		if(index < 0 || index > this.size) {
+			throw new IndexOutOfBoundsException("set: invalid index = " + index);
+		}
+		E toBeReturned = element[index];
+		element[index] = e;
+		return toBeReturned;
 	}
 
 
 	public int size() {
 		return size;
 	}	
-	
 	
 	
 	// private methods  -- YOU CAN NOT MODIFY ANY OF THE FOLLOWING
@@ -113,22 +118,33 @@ public class ArrayIndexList<E> implements IndexList<E> {
 
 	// The following two methods are to be implemented as part of an exercise
 	public Object[] toArray() {
-		//Object[] arrayToReturn = new Object[element.length-1]; 
-		Object[] arrayToReturn = Arrays.copyOf(element, element.length); 	
-		return arrayToReturn;
+		Object[] toBeReturned = new Object[this.size];
+		for(int i=0; i<element.length; i++){
+			toBeReturned[i] = element[i];
+		}
+		return toBeReturned;
 	}
 
 
 	@Override
 	public <T1> T1[] toArray(T1[] array) {
-		T1[] result = (T1[]) Arrays.copyOf(element, element.length, array.getClass());
-		return result; 
+		if(array.length < this.size){
+			array = (T1[]) Array.newInstance(array.getClass().getComponentType(), this.size);
+		}
+		else if(array.length>size){
+			for(int j=this.size; j<array.length;j++){
+				array[j] = null;
+			}
+		}
+		for(int i=0; i<this.size; i++){
+			array[i] = (T1) element[i];
+		}
+		return array;
 	}
-
-
+	
+	
 	@Override
 	public int capacity() {
-		
 		return element.length;
 	}
 
